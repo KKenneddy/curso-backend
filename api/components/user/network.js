9161261f785d25980c4c62c8e64dcd8473e3afc1
Kennedy <nofuequequise@gmail.com>
2022-08-25 15:36:47 -0400

    cap4

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const response = require('../../../network/response');
-
+const controller = require('./controller');
 const router = express.Router();
 
 router.get('/', (req,res)=>{
-    response.success(req, res, 'Todo Correcto', 200);
+    const lista = controller.list();
+    response.success(req, res, lista, 200);
 });
 
 module.exports = router;
